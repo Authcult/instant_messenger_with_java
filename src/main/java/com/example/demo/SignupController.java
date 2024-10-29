c9345d0e88ecbc4191823fa607616b5713e1da7f
罗森 <1994609633@qq.com>
2024-10-29 23:29:33 +0800

    点击相应按钮跳转到相应界面

---
+++ src/main/java/com/example/demo/SignupController.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+public class SignupController {
+    private TextField usernameField;
+    private PasswordField passwordField;
+    private Button confirmbutton;
+}
+
