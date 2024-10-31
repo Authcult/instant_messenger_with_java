f2ec437abf3a3468206e3fcd51d7684e35048e00
Robubu <1994609633@qq.com>
2024-10-31 16:49:28 +0800

    refactor(client): 重构客户端代码并添加新功能
    - 重命名包名和文件路径，统一使用 client 命名空间- 添加忘记密码、登录、注册等功能界面
    - 实现基本的页面跳转逻辑
    - 优化界面样式，调整布局和样式属性

--- src/main/java/client/LoginApp.java
+++ src/main/java/client/LoginApp.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package client;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
--- src/main/java/client/LoginController.java
+++ src/main/java/client/LoginController.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package client;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
