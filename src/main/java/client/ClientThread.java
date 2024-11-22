0f33eb964364b7e9edca88974f9b53f0619864b4
刘楷 <l2950201560@qq.com>
2024-11-22 16:57:40 +0800

    feat(client): 重构客户端并添加好友列表功能
    - 重构了 LoginApp 和 LoginController，将服务器连接逻辑移至 LoginController
    - 新增 MainpageController 中的好友列表功能
    - 修改 ServerThread以支持获取好友列表的请求- 删除了未使用的 Server 类- 优化了消息类型定义，支持新的好友列表相关消息

---
+++ src/main/java/client/ClientThread.java
@@ -0,0 +1,69 @@
+package client;
+
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.List;
+
+public class ClientThread extends Thread{
+
+    private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+    private MessageModel messageModel; // 共享的 MessageModel 实例
+
+    public ClientThread(MessageModel messageModel, Socket socket) {
+        this.messageModel = messageModel; // 保存MessageModel引用
+        this.socket=socket;
+    }
+
+    private ObservableList<String> friendList = FXCollections.observableArrayList();
+
+    public ObservableList<String> getFriendList() {
+        return friendList;
+    }
+    public void updateFriendList(List<String> newFriendList) {
+        Platform.runLater(() -> {
+            friendList.setAll(newFriendList);
+        });
+    }
+    public void connectToServer() {
+        try {
+
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+
+            // 读取服务器消息
+            String serverMessage;
+            while ((serverMessage = in.readLine()) != null) {
+                String finalServerMessage = serverMessage;
+
+                if (finalServerMessage.startsWith(MessageType.SendServerMessage)) {
+                    String data=finalServerMessage.substring(MessageType.SendServerMessage.length()+1);
+                    // 将服务器消息写入 MessageModel
+                    messageModel.setMessage(data);
+                }
+                if (finalServerMessage.startsWith(MessageType.Friendlist)) {
+                    String data=finalServerMessage.substring(MessageType.Friendlist.length()+1);
+                    List<String> friendList = Arrays.asList(data.split(","));
+                    System.out.println(friendList);
+                    updateFriendList(friendList);
+                }
+
+            }
+
+            // 关闭连接
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
