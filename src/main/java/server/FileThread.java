408731c79905959275de0a587b136cd8503677ff
刘楷 <l2950201560@qq.com>
2024-12-02 21:35:08 +0800

    feat(file): 实现文件传输功能
    - 新增文件传输相关的代码和逻辑
    - 在客户端和服务器端分别添加文件发送和接收的方法
    - 修改现有的消息处理流程，支持文件传输
    - 优化用户界面，增加文件发送按钮和相关提示

---
+++ src/main/java/server/FileThread.java
@@ -0,0 +1,89 @@
+package server;
+
+import java.io.*;
+import java.math.RoundingMode;
+import java.net.Socket;
+import java.text.DecimalFormat;
+
+public class FileThread {
+    private Socket socket;
+    private String savePath = "D:/temp_file/";
+
+
+    public FileThread(Socket socket) {
+        this.socket = socket;
+    }
+
+
+    public void work() {
+        try {
+            System.out.println("======== 文件接收开始 ========");
+            System.out.println("文件传输端口号：" + socket.getPort());
+            InputStream inputStream = socket.getInputStream();
+            DataInputStream dataInputStream = new DataInputStream(inputStream);
+
+            String fileName = dataInputStream.readUTF();
+            System.out.println("文件名：" + fileName);
+            long fileSize = dataInputStream.readLong();
+
+            File file = new File(savePath + fileName);
+            FileOutputStream fileOutputStream = new FileOutputStream(file);
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            long remainingBytes = fileSize;
+            while ((bytesRead = dataInputStream.read(buffer, 0, (int) Math.min(buffer.length, remainingBytes))) != -1) {
+                bufferedOutputStream.write(buffer, 0, bytesRead);
+                remainingBytes -= bytesRead;
+                if (remainingBytes == 0) {
+                    break;
+                }
+            }
+
+//            bufferedOutputStream.close();
+//            dataInputStream.close();
+            System.out.println("文件接收完毕，保存路径：" + file.getAbsolutePath());
+        } catch (Exception e) {
+            System.out.println("======== 文件接收过程中发生错误 ========");
+            e.printStackTrace();
+        } finally {
+            try {
+                System.out.println("文件传输线程结束");
+            } catch (Exception e) {}
+        }
+    }
+
+
+    /**
+     * 格式化文件大小
+     * @param length
+     * @return
+     */
+
+    private static DecimalFormat df = null;
+    static {
+        // 设置数字格式，保留一位有效小数
+        df = new DecimalFormat("#0.0");
+        df.setRoundingMode(RoundingMode.HALF_UP);
+        df.setMinimumFractionDigits(1);
+        df.setMaximumFractionDigits(1);
+    }
+
+    private String getFormatFileSize(long length) {
+        double size = ((double) length) / (1 << 30);
+        if(size >= 1) {
+            return df.format(size) + "GB";
+        }
+        size = ((double) length) / (1 << 20);
+        if(size >= 1) {
+            return df.format(size) + "MB";
+        }
+        size = ((double) length) / (1 << 10);
+        if(size >= 1) {
+            return df.format(size) + "KB";
+        }
+        return length + "B";
+    }
+
+}
